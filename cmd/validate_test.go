22aa56788ba3f5928bdf643302952f177c1bcfbb
Mattia Fiumara <mfiumara@protonmail.com>
2023-04-28 00:59:14 +0200

    fix tests

--- cmd/validate_test.go
+++ cmd/validate_test.go
@@ -8,14 +8,14 @@ import (
 
 func TestValidate(t *testing.T) {
 
-	err := Validate("testdata/petstore.json")
+	_, err := Validate("testdata/petstore.json")
 
 	assert.Nilf(t, err, "spec was valid but returned error")
 
-	err = Validate("testdata/petstore.yaml")
+	_, err = Validate("testdata/petstore.yaml")
 	assert.Nilf(t, err, "spec was valid but returned error")
 
-	err = Validate("testdata/invalid.yaml")
+	_, err = Validate("testdata/invalid.yaml")
 	assert.NotNilf(t, err, "expected an error due to invalid spec, but parsed succesfully")
 
 }
