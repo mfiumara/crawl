c0d45ec7da1e03a4d44c59084b11dcd1baaee5f6
Mattia Fiumara <mfiumara@protonmail.com>
2023-04-30 19:41:53 +0200

    feat: initial version with server selection working for GET requests.
    This only works for unparametrized URLs that require no authentication header

--- cmd/prompt.go
+++ cmd/prompt.go
@@ -8,6 +8,8 @@ import (
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/spf13/cobra"
+	"io"
+	"net/http"
 	"os"
 )
 
@@ -29,27 +31,60 @@ to quickly create a Cobra application.`,
 	},
 }
 
+type Path struct {
+	path string
+	item openapi3.PathItem
+}
+
 func Prompt() error {
 
 	// Set the printing format
 	opLen := 0
+	for _, value := range doc.Servers {
+		if len(value.Description) > opLen {
+			opLen = len(value.Description)
+		}
+	}
+	format := fmt.Sprintf("%%-%ds | %%s", opLen)
+
+	var servers []string
+	svrs := make(map[string]openapi3.Server)
+	for _, value := range doc.Servers {
+		choice := fmt.Sprintf(format, value.Description, value.URL)
+		servers = append(servers, choice)
+		svrs[choice] = *value
+	}
+	serverPrompt := &survey.Select{
+		Message: "Select which server to use",
+		Options: servers,
+	}
+	var server string
+	err := survey.AskOne(serverPrompt, &server, survey.WithValidator(survey.Required))
+	if err != nil {
+		println(err.Error())
+		return err
+	}
+	baseUrl := svrs[server].URL
+	// Set the printing format
+	opLen = 0
 	for key, _ := range doc.Paths {
 		if len(key) > opLen {
 			opLen = len(key)
 		}
 	}
-	format := fmt.Sprintf("%%-%ds %%-%ds | %%s", 7, opLen)
+	format = fmt.Sprintf("%%-%ds %%-%ds | %%s", 7, opLen)
 
 	var options []string
-	ops := make(map[string]openapi3.Paths)
+	ops := make(map[string]Path)
 	for key, value := range doc.Paths {
 		for method, operation := range value.Operations() {
 			path := fmt.Sprintf(format, method, key, operation.Summary)
 			options = append(options, path)
-			if ops[path] == nil {
-				ops[path] = make(openapi3.Paths)
+
+			ops[path] = Path{
+				path: key,
+				item: *value,
 			}
-			ops[path][key] = value
 		}
 	}
 	methodPrompt := &survey.Select{
@@ -57,15 +92,23 @@ func Prompt() error {
 		Options: options,
 	}
 	var answer string
-	err := survey.AskOne(methodPrompt, &answer, survey.WithValidator(survey.Required))
+	err = survey.AskOne(methodPrompt, &answer, survey.WithValidator(survey.Required))
 	if err != nil {
 		println(err.Error())
 		return err
 	}
-	for key, value := range ops[answer] {
-		println("You chose ", key, value)
+	resp, err := http.Get(baseUrl + ops[answer].path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
 	}
-
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(string(body))
 	return nil
 }
 
