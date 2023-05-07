4ac2cc84b27b5a0c6c86edfbce6511996218bdc4
Mattia Fiumara <mfiumara@protonmail.com>
2023-05-07 22:48:09 +0200

    maint: 100% coverage on parsing

--- internal/spec/parse_test.go
+++ internal/spec/parse_test.go
@@ -64,11 +64,13 @@ func TestGetOptionsAndPaths(t *testing.T) {
 func TestValidate(t *testing.T) {
 
 	_, err := Validate("../testdata/petstore.json")
-
 	assert.Nilf(t, err, "spec was valid but returned error")
 
 	_, err = Validate("../testdata/petstore.yaml")
 	assert.Nilf(t, err, "spec was valid but returned error")
+
+	_, err = Validate("nonexistent.yaml")
+	assert.NotNilf(t, err, "expected an error due to invalid path, but parsed succesfully")
 
 	_, err = Validate("../testdata/invalid.yaml")
 	assert.NotNilf(t, err, "expected an error due to invalid spec, but parsed succesfully")
